2323fead4df2eb615c3c6e1a3dc5de049bb6e15d
Oksana <cymbaloksana2003@gmail.com>
2023-07-24 22:15:40 +0300

    Update HW#9

--- src/HW9/Main.java
+++ src/HW9/Main.java
@@ -2,13 +2,11 @@ package HW9;
 
 public class Main {
     public static void main(String[] args) {
-        // Используем различные конструкторы для создания объектов Tree
         Tree tree1 = new Tree("Ель", 200);
         Tree tree2 = new Tree(250, 20, "Красный");
         Tree tree3 = new Tree();
         Tree tree4 = new Tree("Дуб");
 
-        // Можно обращаться к полям объектов и использовать их
         System.out.println("Дерево 1: " + tree1.type + ", высота: " + tree1.height + ", веток: " + tree1.countOfSticks + ", цвет: " + tree1.colour);
         System.out.println("Дерево 2: " + tree2.type + ", высота: " + tree2.height + ", веток: " + tree2.countOfSticks + ", цвет: " + tree2.colour);
         System.out.println("Дерево 3: " + tree3.type + ", высота: " + tree3.height + ", веток: " + tree3.countOfSticks + ", цвет: " + tree3.colour);
